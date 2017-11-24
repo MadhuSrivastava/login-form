ad54d2f78b09b9231d01eaac26e419eed18577f2
MadhuSrivastava <madhu.srivastava@qcin.org>
2017-11-24 17:02:38 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,33 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+
+import { RouterModule , Routes } from '@angular/router';
+import { DatatableComponent } from './datatable/datatable.component';
+
+const appRoutes:Routes = [
+  {
+    path: 'login',
+    component:LoginComponent
+  }
+  ,
+  {
+    path: 'datatable',
+    component:DatatableComponent
+  }
+  
+]
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    DatatableComponent,
   ],
   imports: [
+    RouterModule.forRoot(appRoutes),
     BrowserModule
   ],
   providers: [],
